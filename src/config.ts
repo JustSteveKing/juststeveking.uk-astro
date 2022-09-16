2a86b09e14548e314871ad517705db843b70e091
Steve McDougall <juststevemcd@gmail.com>
2022-09-16 20:03:28 +0100

    Making more pages

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,18 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'JustSteveKing';
-export const SITE_DESCRIPTION = 'Consultant CTO, Software Engineer, Educator, Community Advocate';
+export const SITE_DESCRIPTION = 'Consultant CTO, Software Engineer, Educator, Technical Writer, Community Advocate';
+
+// WORK INFO
+export const WORK_TITLE = 'Senior Engineering Manager';
+export const WORK_PLACE = 'Geomiq';
+export const WORK_URL = 'https://www.geomiq.com/';
+export const WORK_DESCRIPTION = "the UK’s leading digital manufacturing marketplace, which is an on-demand manufactoring solution";
+
+// Social Links
+export const LINK_YOUTUBE = 'https://www.youtube.com/c/JustSteveKing';
+export const LINK_LINKEDIN = 'https://www.linkedin.com/in/steve-mcdougall/';
+export const LINK_GITHUB = 'https://github.com/JustSteveKing';
+export const LINK_TWITTER = 'https://twitter.com/JustSteveKing';
+export const LINK_DISCORD = 'https://discord.gg/jkbfSufNTq';
+export const LINK_TELEGRAM = 'https://t.me/JustSteveKing';
