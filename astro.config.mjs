733559ae3391bc61ddba60311a8df551e9ed3e9e
Steve McDougall <juststevemcd@gmail.com>
2022-09-17 19:14:03 +0100

    Working on something awesome

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,34 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import alpinejs from "@astrojs/alpinejs";
+import serviceWorker from "astrojs-service-worker";
+import webmanifest from 'astro-webmanifest';
+import compressor from "astro-compressor";
+
+import {
+  SITE_DESCRIPTION,
+  SITE_TITLE,
+} from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image(), alpinejs()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image(),
+    alpinejs(),
+    compressor(),
+    serviceWorker(),
+    webmanifest({
+      name: SITE_TITLE,
+      description: SITE_DESCRIPTION,
+      start_url: '/',
+      display: 'standalone',
+      theme_color: '#2dd4bf',
+      background_color: '#fafafa'
+    })
+  ]
 });
--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,20 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+// Site General
 export const SITE_TITLE = 'JustSteveKing';
 export const SITE_DESCRIPTION = 'Consultant CTO, Software Engineer, Educator, Technical Writer, Community Advocate';
+export const SITE_URL = 'https://www.juststeveking.uk/';
+
+export const TWITTER_HANDLE = '@JustSteveKing';
 
 // WORK INFO
 export const WORK_TITLE = 'Senior Engineering Manager';
 export const WORK_PLACE = 'Geomiq';
 export const WORK_URL = 'https://www.geomiq.com/';
 export const WORK_DESCRIPTION = "the UK’s leading digital manufacturing marketplace, which is an on-demand manufactoring solution";
+export const WORK_FOUNDED = '2016-08-09';
+export const WORK_LINKEDIN = 'https://www.linkedin.com/company/geomiq/';
+export const WORK_INSTAGRAM = 'https://www.instagram.com/geomiq_platform/';
+export const WORK_TWITTER = 'https://twitter.com/geomiq_official/';
+export const WORK_FACEBOOK = 'https://www.facebook.com/geomiq/';
 
 // Social Links
 export const LINK_YOUTUBE = 'https://www.youtube.com/c/JustSteveKing';
@@ -17,3 +23,11 @@ export const LINK_GITHUB = 'https://github.com/JustSteveKing';
 export const LINK_TWITTER = 'https://twitter.com/JustSteveKing';
 export const LINK_DISCORD = 'https://discord.gg/jkbfSufNTq';
 export const LINK_TELEGRAM = 'https://t.me/JustSteveKing';
+
+// Analytics
+export const FATHOM_SITE = 'SGJKEWOR';
+
+// Schema ORG
+export const SCHEMA_NAME = 'Steve McDougall';
+export const SCHEMA_URL = 'https://www.juststeveking.uk/';
+export const SCHEMA_IMAGE = '';
