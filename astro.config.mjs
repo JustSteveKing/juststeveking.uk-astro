5879c2c26a5209e2413bd8c8aed7e40529073027
Steve McDougall <juststevemcd@gmail.com>
2022-09-18 01:30:52 +0100

    Making a few tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,19 +8,34 @@ import webmanifest from 'astro-webmanifest';
 import compressor from "astro-compressor";
 import robotsTxt from 'astro-robots-txt';
 import { SITE_DESCRIPTION, SITE_TITLE } from './src/config';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juststeveking-uk-astro.pages.dev/',
-  integrations: [mdx(), sitemap(), tailwind(), image(), compressor(), serviceWorker(), robotsTxt(), webmanifest({
-    name: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    start_url: '/',
-    icon: 'https://juststeveking-uk-astro.pages.dev/favicon.svg',
-    display: 'standalone',
-    theme_color: '#2dd4bf',
-    background_color: '#fafafa'
-  }), alpinejs()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image(),
+    compressor(),
+    serviceWorker(),
+    robotsTxt(),
+    alpinejs(),
+    partytown({
+      debug: false,
+      forward: ['fathom'],
+    }),
+    webmanifest({
+      name: SITE_TITLE,
+      description: SITE_DESCRIPTION,
+      start_url: '/',
+      icon: 'https://juststeveking-uk-astro.pages.dev/favicon.svg',
+      display: 'standalone',
+      theme_color: '#2dd4bf',
+      background_color: '#fafafa'
+    })
+  ]
 });
--- src/scripts/index.js
+++
@@ -1,3 +0,0 @@
-export function track(id, value) {
-    console.log(id, value);
-};
