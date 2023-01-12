47d369bfccfb68395022719b4d581d2478652cf7
Steve McDougall <juststevemcd@gmail.com>
2023-01-12 19:33:02 +0000

    Styling stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		...colors,
 		extend: {
 			colors: {
 				'laravel-news': '#ff2d20d9',
